486b7f5e3be83f4f90d08e749b1cd912242561ac
alexhatter1987 <43485116+alexhatter1987@users.noreply.github.com>
2019-02-08 02:36:19 -0600

    RecordMoveToRetraceStepsCreated

---
+++ Example Project Game/Assets/MyReplay.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour {
+
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrame = new MyKeyFrame[bufferFrames];
+    private Rigidbody rB;
+
+
+	// Use this for initialization
+	void Start () {
+        rB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Record();
+    }
+
+    void PlayBack()
+    {
+        rB.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+        print("Reading Frames " + frame);
+        transform.position = keyFrame[frame].postion;
+        transform.rotation = keyFrame[frame].rotation;
+    }
+
+    private void Record()
+    {
+        rB.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+        print("Writing frame " + frame);
+
+        keyFrame[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+}
+
+public struct MyKeyFrame
+{
+
+    public float frameTime;
+    public Vector3 postion;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float atime, Vector3 aPosition, Quaternion aRotation)
+    {
+        frameTime = atime;
+        postion = aPosition;
+        rotation = aRotation;
+    }
+
+
+}
